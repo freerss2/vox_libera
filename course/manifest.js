7ca16186585bb06386a568c72e3a364e59ed0b52
freerss2 <felix.liberman@gmail.com>
2026-03-27 13:00:05 +0300

    rename directory (typo); small fix in lesson;

---
+++ course/manifest.js
@@ -0,0 +1,22 @@
+const manifest = {
+  "id": "arabic_1",
+  "author": "felixl@rambler.ru",
+  "version": "2.7.0",
+  "metadata": {
+    "title": { "ru": "Основы арабского", "en": "Arabic Basics" },
+    "description": {
+        "ru": "Первые 200 слов и базовые конструкции предложения.", 
+        "en": "First 200 words and building basic sentences"},
+    "level": "A0",
+    "prerequisites": ["Alphabet"],
+    "goals": ["Basic Greetings", "Simple Sentences"]
+  },
+  "languages": {
+    "target": "ar",
+    "supported_interfaces": [
+      { "code": "ru" },
+      { "code": "en" }
+    ]
+  },
+  "schema": ["ru", "target", "transcription", "en"]
+}
