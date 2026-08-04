11918b5d637ec39ca2caad1d4d20db6f24864e45
freerss2 <felix.liberman@gmail.com>
2026-08-04 22:58:32 +0300

    fix bug in mobile screen display;

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@
 // common data
 
 const engine_ver = '2.9.3';
-const app_version = '3.3.15';
+const app_version = '3.3.16';
 
 const courses = [
   {"ref": "course.ar1", "code": "ع", "title": "Arabic Basics"},
